9e88e0c7879a992e82afc05f575c29739fe65410
DESKTOP-AMH9RCG\tamas <tamas-kozma@outlook.com>
2022-01-30 00:51:33 +0100

    Fixed button group text alignment in small mode.

--- itslearning-web/src/components/ButtonGroup.svelte
+++ itslearning-web/src/components/ButtonGroup.svelte
@@ -5,7 +5,7 @@
   let klass = '';
   export { klass as class };
 
-  const buttonClasses = "cursor-pointer flex-auto px-2 py-2 h-10 relative hover:bg-background-highlight dark:hover:bg-background-highlight-dark select-none";
+  const buttonClasses = "cursor-pointer flex-auto px-2 py-2 h-10 relative hover:bg-background-highlight dark:hover:bg-background-highlight-dark select-none text-left sm:text-center";
   const selectedButtonClasses = "after:bg-foreground dark:after:bg-foreground-dark after:absolute after:left-0 after:top-0 after:h-full after:w-[2px] sm:after:top-[calc(100%-1px)] sm:after:w-full sm:after:h-[2px]";
 
   $: selectedItem = (items != null && selectedIndex >= 0 && selectedIndex < items.length) ? items[selectedIndex] : null;
